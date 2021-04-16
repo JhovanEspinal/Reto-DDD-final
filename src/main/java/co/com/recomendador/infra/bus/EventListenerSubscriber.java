994576460f211ba96b6a37af00ac7a85916c0aab
Jhovan Espinal <jhovan1028@gmail.com>
2021-04-16 07:26:50 -0500

    Se agrega infrastuctura

---
+++ src/main/java/co/com/recomendador/infra/bus/EventListenerSubscriber.java
@@ -0,0 +1,56 @@
+package org.example.nomemientan.infra.bus;
+
+import co.com.sofka.application.ApplicationEventDrive;
+import co.com.sofka.business.generic.UseCase;
+import co.com.sofka.business.generic.UseCaseHandler;
+import co.com.sofka.business.repository.DomainEventRepository;
+import co.com.sofka.business.support.ResponseEvents;
+import co.com.sofka.business.support.TriggeredEvent;
+import co.com.sofka.domain.generic.DomainEvent;
+import co.com.sofka.infraestructure.asyn.SubscriberEvent;
+import co.com.sofka.infraestructure.event.EventSerializer;
+import co.com.sofka.infraestructure.repository.EventStoreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Component;
+
+
+import java.util.concurrent.Flow;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Component
+public class EventListenerSubscriber implements Flow.Subscriber<DomainEvent> {
+    private static final Logger logger = Logger.getLogger(EventListenerSubscriber.class.getName());
+
+    @Autowired
+    private ApplicationEventPublisher applicationEventPublisher;
+
+    @Autowired
+    private ApplicationEventDrive applicationEventDrive;
+    private Flow.Subscription subscription;
+
+    @Override
+    public void onSubscribe(Flow.Subscription subscription) {
+        subscription.request(1);
+        this.subscription = subscription;
+    }
+
+    @Override
+    public void onNext(DomainEvent event) {
+        logger.log(Level.INFO, "Process event {0}", EventSerializer.instance().serialize(event));
+        applicationEventPublisher.publishEvent(event);
+        applicationEventDrive.fire(event);
+        subscription.request(1);
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+        logger.log(Level.SEVERE, throwable.getMessage());
+    }
+
+    @Override
+    public void onComplete() {
+    }
+
+}
